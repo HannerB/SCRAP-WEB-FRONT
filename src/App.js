8458c0184fc22093156ee28d8dce3b3706fed992
HannerB <hannerb48@gmail.com>
2024-10-14 11:38:40 -0500

    first commit.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ScrapComponent from './components/ScrapComponent'; // Asegúrate de ajustar la ruta según la ubicación real de tu componente
+
+function AppComponent() {
+    return (
+        <div>
+            <ScrapComponent />
+        </div>
+    );
+}
+
+export default AppComponent;
